c90f4164dc4bf85de5c697c106300e306d55443e
Boiko Pavlo <pavlo.boiko@strikersoft.com>
2021-02-20 16:18:46 +0200

    NT20H-2021 updated app

--- app/flask_app.py
+++ app/flask_app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-	return 'Moe Flask приложение в контейнере Docker.'
+	return 'ВЕЛИКИЙ SYSAN делает вещи.'
 
 if __name__ == '__main__':
 	app.run(debug=True,host='0.0.0.0')
